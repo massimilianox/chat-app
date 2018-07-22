cb938881541eb39e1ecf83229c9743da65dee96a
Mamo <m.abeli@t4project.com>
2018-07-22 22:30:13 +0200

    Use SWRevealViewController

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,19 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Massimiliano Abeli on 22/07/2018.
+//  Copyright © 2018 Massimiliano Abeli. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController().rearViewRevealWidth = self.view.frame.width - 60
+    }
+
+}
---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,28 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Massimiliano Abeli on 22/07/2018.
+//  Copyright © 2018 Massimiliano Abeli. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    @IBOutlet weak var titleLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the action touchUpInside with #selector to SWRevealViewController.revealToggle(_:)
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        // Gesture recognizer built in SWRevealViewController
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+    }
+
+}
